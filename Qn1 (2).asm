8da495f6ae04b14851634cb38d1a7335c2554b71
mohith <130842843+mohithkotian@users.noreply.github.com>
2025-04-04 10:43:15 +0530

    Add files via upload

---
+++ Qn1 (2).asm
@@ -0,0 +1,9 @@
+    AREA PRG1, CODE, READONLY  
+    ENTRY                    
+    MOV r1, #0        
+    MOV r3, #12     
+    MVN r0, r1                         
+    AND r4, r0, r2                                            
+                    
+
+    END             ; 
